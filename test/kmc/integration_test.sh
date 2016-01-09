6e81b612ab831ec847da15c2296dc9feec43a002
hoffmannm <mjhoffmann@gmail.com>
2016-01-08 18:15:51 -0800

    Refined kmc integration test

---
+++ test/kmc/integration_test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+# go to directory
+pushd ../../tutorials/2-creating_microkinetic_model 
+
+# run test and grep out critical string
+#for fn in CO_oxidation.mkm CO_oxidation2.mkm CO_oxidation_square.mkm
+for fn in CO_oxidation2.mkm
+do
+    seed=$(basename ${fn} .mkm)
+    outdir="translated_${seed}_local_smart"
+
+    catmap to_kmc ${fn}
+    kmos export translated_${seed}.xml
+
+    cp ${fn} ${outdir}
+    pushd ${outdir}
+
+    catmap run_kmc
+
+
+    popd
+
+done
+
+popd
