60e75e7835a57acb457d1ab9f116b06af8664c3d
hoffmannm <mjhoffmann@gmail.com>
2016-01-11 18:42:31 -0800

    Let integration test to simple model first

--- test/kmc/integration_test.sh
+++ test/kmc/integration_test.sh
@@ -6,7 +6,7 @@ pushd ../../tutorials/2-creating_microkinetic_model
 
 # run test and grep out critical string
 #for fn in CO_oxidation.mkm CO_oxidation2.mkm CO_oxidation_square.mkm
-for fn in CO_oxidation2.mkm
+for fn in CO_oxidation.mkm
 do
     seed=$(basename ${fn} .mkm)
     outdir="translated_${seed}_local_smart"
